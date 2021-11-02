22c053707f722ffeec5782865c0630348cba6e60
Thairan94 <thairansantos2106@gmail.com>
2021-11-02 17:17:47 -0300

    first commit

---
+++ src/components/Mensagem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Mensagem() {
+    return (
+        <h1>Embora ninguém possa voltar atrás e fazer um novo começo,<br></br>qualquer um pode começar agora e fazer um novo fim.</h1>
+    )
+}
+
+export default Mensagem;
